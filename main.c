53f17280f26d38e61b9a5e3afd90d48c4d13a594
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-04-26 03:11:48 +0100

    update main

--- main.c
+++ main.c
@@ -9,26 +9,25 @@
  */
 
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {	int (*builtin)(char **, int, char *);
 	size_t n = 0;
 	char *lineptr = NULL;
 	char **line = argv;
 	int status = 0;
+	int bytes_read;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-		prompt();
-		if (getline(&lineptr, &n, stdin) == -1)
-		{
-			_putchar(10);
+			prompt();
+		bytes_read = getline(&lineptr, &n, stdin);
+		if (bytes_read == -1 || bytes_read == EOF)
 			break;
-		}
-		if ((*lineptr == '\n') || (*lineptr == '\0'))
-			continue;
+		if (lineptr[bytes_read - 1] == '\n')
+			lineptr[bytes_read - 1] = '\0';
 		line = parser(lineptr);
-		if (!line || !line[0])
+		if (line == NULL)
 			continue;
 		builtin = check_builtins(line);
 		if (builtin)
@@ -37,14 +36,11 @@ int main(int argc __attribute__((unused)), char *argv[])
 			free_memory_pp(line);
 			continue;
 		}
-		else
 		execmd(line);
 		free_memory_pp(line);
 	}
 	if (lineptr != NULL)
-	{
 		free(lineptr);
-	}
+	_putchar(10);
 	return (0);
 }
-
