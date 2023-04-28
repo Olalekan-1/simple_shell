2218b4ba3d14b42aeb4f827d7877537c1be943c2
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-04-28 16:16:03 +0100

    updating my exit()

--- exit.c
+++ exit.c
@@ -29,19 +29,23 @@ int __exit(char **cmd, int status, char *filename)
 		return (2);
 	}
 
-	status = _atoi(cmd[1]);
-	if ((!is_numeric(cmd[1])) || status < 0)
-	{
-		_putss(filename);
-		_putss(": ");
-		_putss(cmd[0]);
-		_putss(": ");
-		_putss("Illegal number: ");
-		_putss(cmd[1]);
-		_putchar('\n');
-		return (2);
-	}
-	/*status = atoi(cmd[1]);*/
+
+		status = _atoi(cmd[1]);
+		if ((!is_numeric(cmd[1])) || status < 0)
+		{
+			write(2, filename, _strlen(filename));
+			write(2, ": ", 2);
+			write(2, "1: ", 3);
+			write(2, cmd[0], _strlen(cmd[0]));
+			write(2, ": ", 2);
+			write(2, "illegal number: ", 16);
+			write(2, cmd[1], _strlen(cmd[0]) - 1);
+			_putchar('\n');
+			return (2);
+		}
+	status = atoi(cmd[1]);
 	free_memory_pp(cmd);
 	exit(status);
 }
+
+
